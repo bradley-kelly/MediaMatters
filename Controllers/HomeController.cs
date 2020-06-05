991339c180d17844f61f8253f3ce5e22372a4b0b
BRADLEY KELLY <bradley.kelly@student.centralia.edu>
2020-06-05 12:47:45 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediaMatters.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Albums()
+        {
+            return View();
+        }
+        public IActionResult Books()
+        {
+            return View();
+        }
+        public IActionResult Games()
+        {
+            return View();
+        }
+    }
+}
